77166fe657dd30ef1934bf810b79f2f47e2f92db
bakyt1236 <apsamaov1@gmail.com>
2023-02-18 19:29:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.println("3 Уй Жаныбарлары:");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        String wer = a.nextLine();
+        String wer1 = a.nextLine();
+        String wer2 = a.nextLine();
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("3 Жапайы жаныбар");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        String wer3 = a.nextLine();
+        String wer4 = a.nextLine();
+        String wer5 = a.nextLine();
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("Тоок капа"+"\n"+wer+"\n"+wer1+"\n"+wer2+"\n"+
+               "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"+"\n" +"Жырткычтар"+"\n"+wer3+"\n"
+                +wer4+"\n"+wer5+"\n");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    }
+}
